ead2af278ba0bb7bae725417c5394ee17f71f76d
llmconsortium <llmconsortium@gmail.com>
2025-06-25 13:55:10 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,11 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Bridge',
   description: 'Made by Warren Lee',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
